656590ee9ca5de54aa34cdd91b50d368652602fb
Alan <alin@2u.com>
2017-08-03 08:08:01 -0400

    relative positioning

--- element-tagger.js
+++ element-tagger.js
@@ -1,4 +1,4 @@
-(function (window, $, apiClient) {
+(function (window, $, apiClient, config) {
 
     window.pocketPolice = window.pocketPolice || {};
     window.pocketPolice.elementTagger = {
@@ -19,6 +19,8 @@
             var id = x['item_id'];
             return id;
         });
+        console.log("Marking ids");
+        console.log(ids);
         markTaggedElements(ids);
     }
 
@@ -30,6 +32,7 @@
         $stagedElements.forEach(function ($element) {
             var id = $element.attr('src');
             console.log("Tagging element with id " + id);
+            markElement(id);
             apiClient.postTagRequest(id);
         });
         resetStaging();
@@ -46,11 +49,95 @@
     }
 
     function markElement(id) {
-        console.log("marking element with id " + id);
-        var src = 'https://media1.britannica.com/eb-media/65/61865-004-A58B1676.jpg';
+        var src = chrome.extension.getURL("prison-cell-bars.png");
         var img = $(document).find('img[src$="' + id + '"]');
-        $(img).attr('src', src);
-        $(img).attr('srcset', src);
+        if (img.length) {
+            console.log("Image found with ID: " + id);
+            var $img = $(img);
+            if (ignoreElement($img)) {
+                return;
+            }
+
+
+
+            var $overlay = $('<div/>');
+            $overlay.css({
+                position: 'relative'
+            });
+            var overlayImage = $('<img/>', {
+                class: 'pp-overlay',
+                src: src,
+                css: {
+                    top: $img.position().top,
+                    left: $img.position().left,
+                    position: 'absolute',
+                    width: $img.width(),
+                    height: $img.height(),
+                    'pointer-events': 'none'
+                }
+            });
+            $img.wrap($overlay);
+            $img.after(overlayImage);
+        }
     }
 
-})(window, $, window.pocketPolice.apiClient);
+    function ignoreElement($element) {
+        console.log("----------- ignoreElement()")
+        var ignoreClasses = config.element_ignore.classes;
+        var elementClasses = $element.attr("class").split(/\s+/);
+        console.log("Element classes: ");
+        console.log(elementClasses);
+        console.log("ignore classes:");
+        console.log(ignoreClasses);
+        for (var i = 0; i < ignoreClasses; i++) {
+            var ignoreClass = ignoreClass[i];
+            if (elementClasses.indexOf(ignoreClass) >= 0) {
+                console.log("Ignore!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // function markElement(id) {
+    //     console.log("marking element with id " + id);
+    //     // var src = 'https://media1.britannica.com/eb-media/65/61865-004-A58B1676.jpg';
+    //     var src = chrome.extension.getURL("prison-cell-bars.png");
+    //     var img = $(document).find('img[src$="' + id + '"]');
+    //     if (img.length) {
+    //         var position = $(img).offset();
+    //         // $(img).attr('src', src);
+    //         // $(img).attr('srcset', src);
+    //         // var $overlay = $('<div id="wtf"></div>');
+    //         // $overlay.css({
+    //         //     position: 'relative'
+    //         // });
+    //         // $overlay.css({
+    //         //     display: 'block',
+    //         //     width: $(img).width(),
+    //         //     height: $(img).height(),
+    //         //     backgroundPosition: 'center center',
+    //         //     backgroundImage: "url(" + src + ")"
+    //         // });
+    //         var $img = $(img);
+    //         var overlayImg = $('<img/>', {src: src, css: {
+    //             width: $(img).width(),
+    //             height: $(img).height(),
+    //             padding: $img.css('padding-top') + ' ' + $img.css('padding-right') + ' ' + $img.css('padding-bottom') + ' ' + $img.css('padding-left'),
+    //             display: 'none'
+    //         }});
+    //         $('body').append(overlayImg);
+    //         overlayImg.css({'display': 'block'});
+    //         overlayImg.offset(position);
+    //         overlayImg.show();
+    //         // $(img).wrap($overlay);
+    //         // console.log(overlayImg);
+    //         // $('#wtf').append(overlayImg);
+    //     }
+    // }
+
+})(window, $, window.pocketPolice.apiClient, {
+    'element_ignore': {
+        'classes': ['pp-overlay']
+    }
+});
