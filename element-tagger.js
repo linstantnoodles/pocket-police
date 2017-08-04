28f08e612a4d4b0c728f25a42a5cc7bf5b5b998a
Alan <alin@2u.com>
2017-08-04 02:39:25 -0400

    use hash as optimization and fix blank images by setting pos after load

--- element-tagger.js
+++ element-tagger.js
@@ -7,7 +7,7 @@
         tagStagedElements: tagStagedElements
     };
 
-    var markedElementIds = [];
+    var taggedElements = {};
     var $stagedElements = [];
 
     function initialize() {
@@ -15,54 +15,40 @@
         addEventListeners();
     }
 
-    function addEventListeners() {
-        $(document).mousemove(function (event) {
-            markTaggedElements(markedElementIds);
-        });
-    }
-
     function taggedItemsRequestHandler(status, responseText) {
         var items = JSON.parse(responseText);
-        var ids = items.map(function(x) {
-            var id = atob(x['item_id']);
-            return id;
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            var id = atob(item['item_id']);
+            taggedElements[id] = {};
+        }
+        markTaggedElements();
+    }
+
+    function addEventListeners() {
+        $(document).mousemove(function (event) {
+            markTaggedElements();
         });
-        markedElementIds = markedElementIds.concat(ids);
-        markTaggedElements(ids);
     }
 
-    function stageElementForTagging (element) {
-        $stagedElements = [element];
-    }
-
-    function tagStagedElements () {
-        $stagedElements.forEach(function ($element) {
-            var id = $element.attr('src');
-            markedElementIds.push(id);
-            markElement(id);
-            apiClient.postTagRequest(id);
-        });
-        resetStaging();
-    }
-
-    function resetStaging() {
-        $stagedElements = [];
-    }
-
-    function markTaggedElements(ids) {
-        ids.forEach(function(x) {
-            markElement(x);
-        });
+    function markTaggedElements() {
+        for (elementId in taggedElements) {
+            if (taggedElements.hasOwnProperty(elementId)) {
+                markElement(elementId);
+            }
+        }
     }
 
     function markElement(id) {
-        var img = $(document).find('img[src$="' + id + '"]');
-        if (img.length) {
-            var $img = $(img);
-            if (ignoreElement($img)) {
+        var $img = $(document).find('img[src*="' + id + '"]');
+        if ($img.length) {
+            if (ignoreElement($img) || elementHasOverlay(id)) {
                 return;
             }
             addOverlayToImage($img);
+            updateHasOverlay(id, true);
+        } else {
+            updateHasOverlay(id, false);
         }
     }
 
@@ -71,6 +57,18 @@
         var originalSrc = $element.attr('src');
         var originalWidth = $element.width();
         var originalHeight = $element.height();
+        if (originalWidth <= 0 || originalHeight <= 0) {
+            $element.on('load', function(event) {
+                var width = $element.width();
+                var height = $element.height();
+                $element.width(width);
+                $element.height(height);
+                $element.css({
+                    'background-size': width + 'px' + ' ' + height + 'px'
+                });
+                $(this).off(event);
+            });
+        }
         $element.hide(0, function () {
             $element.addClass('pp-overlay');
             $element.attr('src', config.overlay_img.src);
@@ -81,9 +79,37 @@
                 'background-size': originalWidth + 'px' + ' ' + originalHeight + 'px'
             });
             $element.animate({
-                width:'show'
+                width: 'show'
             }, 500);
         });
+    }
+
+    function elementHasOverlay(id) {
+        var taggedElement = taggedElements[id];
+        return taggedElement && taggedElements[id].has_overlay === true;
+    }
+
+    function updateHasOverlay(id, value) {
+        taggedElements[id] = {
+            has_overlay: value
+        };
+    }
+
+    function stageElementForTagging (element) {
+        $stagedElements = [element];
+    }
+
+    function tagStagedElements () {
+        $stagedElements.forEach(function ($element) {
+            var id = $element.attr('src');
+            markElement(id);
+            apiClient.postTagRequest(id);
+        });
+        resetStaging();
+    }
+
+    function resetStaging() {
+        $stagedElements = [];
     }
 
     function ignoreElement($element) {
