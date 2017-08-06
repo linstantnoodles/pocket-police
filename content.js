12a6c42815726968ff5a55be0d3337b45e133d79
Alan <alin@2u.com>
2017-08-06 15:46:13 -0400

    add private list

--- content.js
+++ content.js
@@ -1,13 +1,4 @@
-(function (window, button, elementTagger, elementWatcher, apiClient) {
-    function getRandomToken() {
-        var randomPool = new Uint8Array(32);
-        crypto.getRandomValues(randomPool);
-        var hex = '';
-        for (var i = 0; i < randomPool.length; ++i) {
-            hex += randomPool[i].toString(16);
-        }
-        return hex;
-    }
+(function (window, button, elementTagger, elementWatcher, apiClient, config) {
 
     chrome.storage.sync.get('userid', function(items) {
         var userid = items.userid;
@@ -24,15 +15,66 @@
     });
 
     function initialize(userid) {
+        var hostname = window.location.hostname;
+        var privateSites = config.private_sites;
+        for (var i = 0; i < privateSites.length; i++) {
+            console.log(hostname);
+            console.log(privateSites[i]);
+            if (hostname.match(privateSites[i])) {
+                return;
+            }
+        }
         apiClient.initialize(userid);
         elementWatcher.initialize();
         elementTagger.initialize();
         button.initialize();
     }
+
+    function getRandomToken() {
+        var randomPool = new Uint8Array(32);
+        crypto.getRandomValues(randomPool);
+        var hex = '';
+        for (var i = 0; i < randomPool.length; ++i) {
+            hex += randomPool[i].toString(16);
+        }
+        return hex;
+    }
+
 })(
     window,
     window.pocketPolice.button,
     window.pocketPolice.elementTagger,
     window.pocketPolice.elementWatcher,
-    window.pocketPolice.apiClient
+    window.pocketPolice.apiClient,
+    {
+        private_sites: [
+            /^(.*?\.|)craigslist\.org/,
+            /^(.*?\.|)chase\.com/,
+            /^(.*?\.|)facebook\.com/,
+            /^(.*?\.|)mail\.aol\.com/,
+            /^(.*?\.|)atmail\.com/,
+            /^(.*?\.|)contactoffice\.com/,
+            /^(.*?\.|)fastmail\.fm/,
+            /^(.*?\.|)webmail\.gandi\.net/,
+            /^(.*?\.|)accounts\.google\.com/,
+            /^(.*?\.|)mail\.google\.com/,
+            /^(.*?\.|)docs\.google\.com/,
+            /^(.*?\.|)gmx\.com/,
+            /^(.*?\.|)hushmail\.com/,
+            /^(.*?\.|)laposte\.fr/,
+            /^(.*?\.|)mail\.lycos\.com/,
+            /^(.*?\.|)mail\.com/,
+            /^(.*?\.|)mail\.ru/,
+            /^(.*?\.|)opolis\.eu/,
+            /^(.*?\.|)outlook\.com/,
+            /^(.*?\.|)nokiamail\.com/,
+            /^(.*?\.|)apps\.rackspace\.com/,
+            /^(.*?\.|)rediffmail\.com/,
+            /^(.*?\.|)runbox\.com/,
+            /^(.*?\.|)mail\.sify\.com/,
+            /^(.*?\.|)webmail\.thexyz\.com/,
+            /^(.*?\.|)mail\.yahoo\.com/,
+            /^(.*?\.|)mail\.yandex\.com/
+        ]
+    }
 );
